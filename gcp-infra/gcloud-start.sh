2f1cf3461eabe13442e28222dbdb40b82b2c100a
Aniket <aniketsingh2151@gmail.com>
2026-09-18 22:13:35 +0530

    chore(gcp): remove legacy GKE manifests targeting decommissioned project

--- gcp-infra/gcloud-start.sh
+++
@@ -1,5 +0,0 @@
-gcloud compute instances resume k8s-control-plane --zone=asia-south2-a
-gcloud compute instances resume k8s-worker-1 --zone=asia-south2-a
-gcloud compute instances resume k8s-worker-2 --zone=asia-south2-a
-
-gcloud sql instances patch statusbus-postgres --activation-policy=ALWAYS
--- gcp-infra/kubectl_script.sh
+++
@@ -1,2 +0,0 @@
-gcloud compute ssh k8s-control-plane --zone asia-south2-a --tunnel-through-iap \
-    -- -N -L 8443:127.0.0.1:6443
